93e2912920fc764c15cdb8b25fab49c9cdafed3a
lukashais <lukyhais@seznam.cz>
2018-11-21 19:41:01 +0100

    test commit

--- updater.sh
+++ updater.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# t
 yes | command cp -r ~/.config/nvim/colors ~/.config/nvim/bundle ~/.config/nvim/autoload ~/.config/nvim/init.vim ~/dotfiles/vim
 yes | command cp -r ~/.chunkwmrc ~/.skhdrc ~/.zshrc ~/.config/neofetch ~/dotfiles
