d6674d6aab6025c758240e77363f85675710f097
lukashais <lukyhais@seznam.cz>
2018-11-21 19:58:25 +0100

    return

--- updater.sh
+++ updater.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# t
 yes | command cp -r ~/.config/nvim/colors ~/.config/nvim/bundle ~/.config/nvim/autoload ~/.config/nvim/init.vim ~/dotfiles/vim
 yes | command cp -r ~/.chunkwmrc ~/.skhdrc ~/.zshrc ~/.config/neofetch ~/dotfiles
