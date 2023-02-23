a9fddc5fccf1cf21fb291c71b5a733b75af30e38
Cr4ckonosh <lukashais@getgoogleoff.me>
2023-02-23 13:46:52 +0100

    quickfix

--- updater.sh
+++ updater.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 OS=$(command uname -s)
+DOTFILES_FOLDER=$(dirname "$0")
 if [[ $OS == "Darwin" ]];
 then
     if [[ -f $HOME/Library/Application\ Support/Code/User/settings.json ]];
     then
-        yes | command cp $HOME/Library/Application\ Support/Code/User/settings.json ~/dotfiles/vscode
+        yes | command cp $HOME/Library/Application\ Support/Code/User/settings.json $DOTFILES_FOLDER/dotfiles/vscode
     else
         echo "Unable to find file '\$HOME/Library/Application\ Support/Code/User/settings.json', skipping..."
     fi
     if [[ -f $HOME/.yabairc ]] && [[ -f $HOME/.skhdrc ]];
     then
-        yes | command cp -r ~/.yabairc ~/.skhdrc ~/dotfiles
+        yes | command cp -r $HOME/.yabairc $HOME/.skhdrc $DOTFILES_FOLDER/dotfiles
     else
         echo "Unable to find '.yabairc' and/or '.skhdrc' in '\$HOME' folder, skipping..."
     fi
@@ -19,7 +20,7 @@ elif [[ $OS == "Linux" ]];
 then
     if [[ -f $HOME/.config/Code/User/settings.json ]];
     then
-        yes | command cp $HOME/.config/Code/User/settings.json ~/dotfiles/vscode
+        yes | command cp $HOME/.config/Code/User/settings.json $DOTFILES_FOLDER/dotfiles/vscode
     else
         echo "Unable to find file '\$HOME/.config/Code/User/settings.json', skipping..."
     fi
@@ -30,15 +31,15 @@ fi
 
 if [[ -d $HOME/.config/nvim ]];
 then
-    yes | command cp -r ~/.config/nvim/colors ~/.config/nvim/autoload ~/.config/nvim/init.vim  ~/dotfiles/vim
-    yes | command cp -r ~/.config/nvim/bundle/README.md ~/dotfiles/vim/bundle
+    yes | command cp -r $HOME/.config/nvim/colors $HOME/.config/nvim/autoload $HOME/.config/nvim/init.vim  $DOTFILES_FOLDER/dotfiles/vim
+    yes | command cp -r $HOME/.config/nvim/bundle/README.md $DOTFILES_FOLDER/dotfiles/vim/bundle
 else
     echo "Unable to find folder '\$HOME/.config/nvim', skipping..."
 fi
 
 if [[ -f $HOME/.zshrc ]];
 then
-    yes | command cp -r ~/.zshrc ~/dotfiles
+    yes | command cp -r $HOME/.zshrc $DOTFILES_FOLDER/dotfiles
 else
     echo "Unable to find file '\$HOME/.zshrc', skipping..."
 fi
